90cb70862b0eb391aacefa38c5ff81490653c4ec
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-04-15 01:38:53 +0700

    Set font face to Fira Code

--- init.el
+++ init.el
@@ -93,8 +93,8 @@ before layers configuration."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font. `powerline-scale' allows to quickly tweak the mode-line
    ;; size to make separators look not too crappy.
-   dotspacemacs-default-font '("Iosevka"
-                               :size 15
+   dotspacemacs-default-font '("Fira Code"
+                               :size 14
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)

693c407b6068c706e2673140cdf3a08e4842efc9
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-04-16 00:37:29 +0700

    Enabled auto-complete

--- layers/wk-j/litable.el
+++ layers/wk-j/litable.el
@@ -1,21 +1,5 @@
-(+ 2 2 3)
-(+ 2 (+ 1 1))
-(+ 3 (+ 2 2))
-
-(* 2 3)
-
-(defun func (arg1 arg2)
-  "Always document your functions.")
-
-(defvar var-name 1
-  "Always document your variables.")
-
-(let ((x 1)
-      (y 2)))
-
-(let* ((x 1)
-       (y 5))
-  (+ x y))
+(+ 1 1 1 1 1 1)
+(+ 1 2)
 
 
 

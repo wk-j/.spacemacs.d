4c86ee68ecc80630dfa14b71ea84d31accb9c180
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-04-17 16:52:17 +0700

    Fixed parinfer

--- layers/wk-j/packages.el
+++ layers/wk-j/packages.el
@@ -31,6 +31,7 @@
 
 (defconst wk-j-packages
   '(all-the-icons
+    parinfer
     (litable :location (recipe
                         :fetcher github
                         :repo "fuco1/litable"))
@@ -46,5 +47,27 @@
 
 (defun wk-j/init-cake-mode ()
   (use-package cake-mode))
+
+;; Parinfer
+(defun wk-j/init-parinfer ()
+  (use-package parinfer
+    :ensure t
+    :bind
+    (("C-," . parinfer-toggle-mode))
+    :init
+    (progn
+      (setq parinfer-extensions
+            '(defaults       ; should be included.
+              pretty-parens  ; different paren styles for different modes.
+              evil           ; If you use Evil.
+              lispy          ; If you use Lispy. With this extension, you should install Lispy and do not enable lispy-mode directly.
+              paredit        ; Introduce some paredit commands.
+              smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
+              smart-yank))   ; Yank behavior depend on mode.
+      (add-hook 'clojure-mode-hook #'parinfer-mode)
+      (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
+      (add-hook 'common-lisp-mode-hook #'parinfer-mode)
+      (add-hook 'scheme-mode-hook #'parinfer-mode)
+      (add-hook 'lisp-mode-hook #'parinfer-mode))))
 
 ;; packages.el ends here
